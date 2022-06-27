ac96506970057471ec2976426c9e08326abb74b9
shireeshakongara <shireesha.kongara@gmail.com>
2022-06-27 11:08:35 +0530

    First

---
+++ SampleDemo/src/main/java/com/spring/SampleDemo/App.java
@@ -0,0 +1,13 @@
+package com.spring.SampleDemo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
